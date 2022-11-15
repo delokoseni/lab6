65f15b3633044684df6f339e2802799eb30e4064
delokoseni <artur.b3lsh@gmail.com>
2022-11-15 10:44:31 +0700

    Перенесен файл .h класса Salary. Перенесена функция проверки расширения файла и дружественная функция для классов Hours и Jobtitle - normhcost

--- lab6/Salary.h
+++ lab6/Salary.h
@@ -1,5 +1,30 @@
 #pragma once
+#include "Experience.h"
+#include "Jobtitle.h"
+
 class Salary
 {
+public:
+    Salary(int overtimecost, int weekendcost, int exppercent, int subspercent, bool expstatus, \
+        bool subsstatus);
+    Salary(int allfields);
+    Salary();
+    void set(Salary salary);
+    void input();
+    void output();
+    int overtimeweekends(int overtime, int weekends);
+    int allmoney(int salary, Experience experience, Jobtitle jobtitle);
+    Salary operator + (Salary salary);
+    Salary& operator++(); //префиксный
+    Salary operator++(int); //постфиксный
+    void tofile(ofstream& file, string filename);
+    void getfromfile(ifstream& file, string filename);
+private:
+    int overtimecost; //Процент увеличения оплаты сверхурочных часов 
+    int weekendcost; //Процент увеличения оплаты часов отработанных в выходные
+    int exppercent; //Процент увеличения оплаты за стаж
+    int subspercent; //Процент увеличения оплаты за наличие подчененных
+    bool expstatus; //Учет стажа
+    bool subsstatus; //Учет наличия подчиненных
 };
 
